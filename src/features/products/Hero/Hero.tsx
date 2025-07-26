108f2b5133c1dcf27bfb8590e27f981d809132d6
osama shehta <osama.shehtafathi@gmail.com>
2025-07-26 23:14:35 +0300

    Hero section with swiper

--- src/features/products/Hero/Hero.tsx
+++ src/features/products/Hero/Hero.tsx
@@ -1,17 +1,42 @@
-"use client"
+"use client";
 import { ProductI } from "../types/products";
-// import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import Image from "next/image";
+import Link from "next/link";
 interface HeroProps {
-    gallerySwiper: ProductI[];
+  gallerySwiper: ProductI[];
 }
 const Hero = ({ gallerySwiper }: HeroProps) => {
   return (
-    <div>Hero</div>
-  )
-}
+    <Swiper
+      navigation={true}
+      modules={[Navigation]}
+      className="mySwiper w-screen h-dvh"
+    >
+      {gallerySwiper.map((slide) => (
+        <SwiperSlide key={slide.id} className="relative w-full h-full">
+          <Link
+            href={`/products/${slide.id}`}
+            className="block w-full h-full relative"
+          >
+            <Image
+              src={slide.image}
+              alt={slide.title}
+              fill
+              sizes="100vw"
+              className="object-contain"
+              priority
+            />
+          </Link>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
 
-export default Hero
+export default Hero;
